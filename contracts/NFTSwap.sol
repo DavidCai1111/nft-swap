781b0e18d923623cd56dc6f31826c50755048883
David Cai <davidcai1993@yahoo.com>
2021-12-23 14:18:29 +0800

    feat: init the swap

--- contracts/NFTSwap.sol
+++ contracts/NFTSwap.sol
@@ -9,16 +9,18 @@ import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NFTSwap is Ownable, ERC721Holder, ERC1155Holder {
-  uint128 private _fee;
-  uint128 private _feeLocked;
+  uint256 private _fee;
+  uint256 private _ethLocked;
 
   // global swaps storage, id => swap
+  uint256 private _swapIdx;
   mapping (uint256 => Swap) private _swaps;
 
   // NFT info, the contract address and NFT id
   struct NFT {
     address contractAddr;
     uint256 id;
+    uint256 amount; // used for ERC-1155
   }
 
   // Both users are allowed to use NTFs and ETHs in the swap
@@ -27,22 +29,22 @@ contract NFTSwap is Ownable, ERC721Holder, ERC1155Holder {
     // User A's swap information
     address payable aAddress;
     NFT[] aNFTs;
-    uint128 aEth;
+    uint256 aEth;
 
     // User B's swap information
     address payable bAddress;
     NFT[] bNFTs;
-    uint128 bEth;
+    uint256 bEth;
   }
 
-  event FeeChange(int128 fee);
+  event FeeChange(uint256 fee);
   // SwapCreate will be emitted when user A create a swap
   event SwapCreate(
     address indexed a,
     address indexed b,
     uint256 indexed id,
     NFT[] aNFTs,
-    uint128 aEth
+    uint256 aEth
   );
   // SwapReady will be emitted when user B finish selecting his/her NFTs and ETHs to swap
   event SwapReady(
@@ -50,9 +52,9 @@ contract NFTSwap is Ownable, ERC721Holder, ERC1155Holder {
     address indexed b,
     uint256 indexed id,
     NFT[] aNFTs,
-    uint128 aEth,
+    uint256 aEth,
     NFT[] bNFTs,
-    uint128 bEth
+    uint256 bEth
   );
   // SwapCancel will be emitted when either side cancel the swap
   event SwapCancel(
@@ -66,4 +68,89 @@ contract NFTSwap is Ownable, ERC721Holder, ERC1155Holder {
     address indexed b,
     uint256 indexed id
   );
+
+  modifier onlyA(uint256 swapId) {
+    require(_swaps[swapId].aAddress == msg.sender, "onlySwapCreatorCanCall");
+    _;
+  }
+
+  modifier chargeFee() {
+    require(msg.value >= _fee, "feeNotGiven");
+    _;
+  }
+
+  constructor(uint128 fee, address owner) {
+    _fee = fee;
+    super.transferOwnership(owner);
+  }
+
+  // Change the contract service fee
+  function changeFee(uint128 fee) external onlyOwner {
+    _fee = fee;
+    emit FeeChange(_fee);
+  }
+
+  // User A create a swap
+  function createSwap(address bAddress, NFT[] memory aNFTs) external payable chargeFee {
+    _swapIdx += 1;
+
+    safeTransfer(msg.sender, address(this), aNFTs);
+
+    Swap storage swap = _swaps[_swapIdx];
+
+    swap.aAddress = payable(msg.sender);
+    swap.aNFTs = aNFTs;
+
+    if (msg.value > _fee) {
+      swap.aEth = msg.value - _fee;
+      _ethLocked += swap.aEth;
+    }
+
+    swap.bAddress = payable(bAddress);
+
+    emit SwapCreate(msg.sender, swap.bAddress, _swapIdx, aNFTs, swap.aEth);
+  }
+
+  // User B init the swap
+  function initSwap(uint256 id, NFT[] memory bNFTs) external payable chargeFee {
+    require(_swaps[id].bAddress != msg.sender, "notCorrectUserB");
+    require(_swaps[id].bNFTs.length == 0 && _swaps[id].bEth == 0, "swapAlreadyInit");
+
+    safeTransfer(msg.sender, address(this), bNFTs);
+
+    _swaps[id].bAddress = payable(msg.sender);
+    _swaps[id].bNFTs = bNFTs;
+
+    if (msg.value > _fee) {
+      _swaps[id].bEth = msg.value - _fee;
+      _ethLocked += _swaps[id].bEth;
+    }
+
+    emit SwapReady(
+      _swaps[id].aAddress,
+      _swaps[id].bAddress,
+      id,
+      _swaps[id].aNFTs,
+      _swaps[id].aEth,
+      _swaps[id].bNFTs,
+      _swaps[id].bEth
+    );
+  }
+
+  function safeTransfer(address from, address to, NFT[] memory nfts) internal {
+    for (uint256 i = 0; i < nfts.length; i++) {
+      // ERC-20 transfer
+      if (nfts[i].amount == 0) {
+        IERC721(nfts[i].contractAddr).safeTransferFrom(from, to, nfts[i].id, "");
+      } else { // ERC-1155 transfer
+        IERC1155(nfts[i].contractAddr).safeTransferFrom(from, to, nfts[i].id, nfts[i].amount, "");
+      }
+    }
+  }
+
+  function withdrawFee(address payable recipient) external onlyOwner {
+    require(recipient != address(0), "canNotWithdrawToAddress0");
+
+    recipient.transfer(address(this).balance - _ethLocked);
+  }
 }
